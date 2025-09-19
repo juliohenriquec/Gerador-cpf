207e93a478ab0753726295eb15282e0c4adc775d
juliohenriquec <juliohm37@outlook.com>
2025-09-19 18:24:59 -0300

    Projeto pronto

--- validacao-cpf.py
+++ validacao-cpf.py
@@ -1,44 +1,28 @@
-import sys
-cpf = input('Digite o cpf: ').replace('.', '-').replace('-', '')
+import random
 
-entrada_repetida = cpf == cpf[0] * len(cpf)
-if entrada_repetida:
-    print('cpf invalido')
-    sys.exit()
+nove_digitos = ''
+for i in range(9):
+    nove_digitos += str(random.randint(0,9))
 
 contador_regressivo_1 = 10
+
 resultado_digito_1 = 0
-nove_digitos = cpf[0:9]
-for digito_1 in range(0,9):
-    resultado_digito_1 += int(cpf[digito_1]) * contador_regressivo_1
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
     contador_regressivo_1 -= 1
-
 digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
 
-if digito_1 > 9:
-    resultado = 0
-else:
-    resultado = digito_1
-
+dez_digitos = nove_digitos + str(digito_1)
 contador_regressivo_2 = 11
+
 resultado_digito_2 = 0
-
-for digito_2 in range(0,10):
-    resultado_digito_2 += int(cpf[digito_2]) * contador_regressivo_2
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
     contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
 
-digito_2 = (resultado_digito_2 * 10) %  11
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
 
-if digito_2 > 9:
-    resultado2 = 0
-else:
-    resultado2 = digito_2
-
-cpf_gerado_calculo = f'{nove_digitos}{digito_1}{digito_2}'
-
-print(f'O primeiro digito {resultado}')
-print(f'O primeiro digito {resultado2}')
-if cpf == cpf_gerado_calculo:
-    print('CPF válido')
-else:
-    print('CPF invalido')
+print(cpf_gerado_pelo_calculo)
